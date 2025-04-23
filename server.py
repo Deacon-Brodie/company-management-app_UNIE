c0392c885e2292bc5cbbcd363507064b85232bba
Tito Choque <alva.tacs@gmail.com>
2025-04-24 00:36:56 +0200

    Cambios para corregir la vulnerabilidad:
    Finding #2: Insecure Session Cookie

--- server.py
+++ server.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 app.secret_key = 'supersecretkey'
 app.permanent_session_lifetime = 99999999
 
+# Configuración segura centralizada, donde declaramos el uso de SECURE, debido a que HttpOnly esta activado por defecto.
+app.config.update(
+    SESSION_COOKIE_SECURE=True,
+)
